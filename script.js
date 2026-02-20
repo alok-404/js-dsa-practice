24d8f4184195f0a34cb98320760f61886a82e4fd
alok-404 <alokchandra2621@gmail.com>
2026-02-20 22:25:08 +0530

    Tried to learn more with Strings

--- script.js
+++ script.js
@@ -1,47 +1,51 @@
-// Q1. Reverse a string without using .reverse()
+// // Q2. Find the second largest number in an array
 
-// Input: "hello"
-// Output: "olleh"
+// let arr = [3, 100, 2, 8, 27, 95, 90, 200];
 
-let string = "hello"
+// if (arr.length < 2) {
+//   alert("array must be at least of 2 elements");
+// }
 
-for(let i = string.length-1 ; i>=0;i--){
-    console.log(string[i]);
+// let largest, secondLargest;
+
+// if (arr[0] > arr[1]) {
+//   largest = arr[0];
+//   secondLargest = arr[1];
+// } else {
+//   largest = arr[1];
+//   secondLargest = arr[0];
+// }
+
+// // console.log(largest);
+// // console.log(secondLargest);
+
+// for (let i = 2; i < arr.length; i++) {
+//   let num = arr[i];
+//   // console.log(num);
+//   if (num > largest) {
+//     secondLargest = largest;
+//     largest = num;
+//   } else if (num > secondLargest) {
+//     secondLargest = num;
+//   }
+// }
+// console.log("largest number is =" + largest);
+// console.log("Second largest number is =" + secondLargest);
+
+// Q4. Check if a string is palindrome
+
+// Input: "madam" → true
+// Input: "hello" → false
+
+let string = "madam";
+let reverse = "";
+
+for (let i = string.length-1; i >= 0; i--) {
+//   console.log(string[i]);
+  reverse += string[i]
 }
 
-// Q2. Find the second largest number in an array
-
-
-let arr = [3, 100, 2, 8, 27, 95, 90, 200];
-
-if (arr.length < 2) {
-  alert("array must be at least of 2 elements");
-}
-
-let largest, secondLargest;
-
-if (arr[0] > arr[1]) {
-  largest = arr[0];
-  secondLargest = arr[1];
-} else {
-  largest = arr[1];
-  secondLargest = arr[0];
-}
-
-// console.log(largest);
-// console.log(secondLargest);
-
-for (let i = 2; i < arr.length; i++) {
-  let num = arr[i];
-  // console.log(num);
-  if (num > largest) {
-    secondLargest = largest;
-    largest = num;
-  } else if (num > secondLargest) {
-    secondLargest = num;
-  }
-}
-console.log("largest number is =" + largest);
-console.log("Second largest number is =" + secondLargest);
+if(string === reverse) console.log(`${string} is Palindrome`);
+else console.log(`${string} is not Palindrome`);
 
 
--- string.js
+++ string.js
@@ -3,8 +3,24 @@
 // Input: "hello"
 // Output: "olleh"
 
-let string = "hello"
+let str = "hello"
 
-for(let i = string.length-1 ; i>=0;i--){
-    console.log(string[i]);
+for(let i = str.length-1 ; i>=0;i--){
+    console.log(str[i]);
 }
+
+// Q4. Check if a string is palindrome
+
+// Input: "madam" → true
+// Input: "hello" → false
+
+let string = "madam";
+let reverse = "";
+
+for (let i = string.length-1; i >= 0; i--) {
+//   console.log(string[i]);
+  reverse += string[i]
+}
+
+if(string === reverse) console.log(`${string} is Palindrome`);
+else console.log(`${string} is not Palindrome`);
