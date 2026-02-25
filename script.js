bc3731d15558e8e4fdfb6d635da29a9d34aa8e4e
alok-404 <alokchandra2621@gmail.com>
2026-02-25 20:36:07 +0530

    Implemented stack and hashing problems with proper edge case handling and dry runs

--- script.js
+++ script.js
@@ -36,10 +36,68 @@
 // console.log(isValid("([)]"));   // false
 // console.log(isValid("((("));    // false
 
+// function firstUniqChar(s) {
+//   let obj = {};
+
+//   for (i = 0; i < s.length; i++) {
+//     let char = s[i];
+//     if (obj[char]) {
+//       obj[char]++;
+//     } else {
+//       obj[char] = 1;
+//     }
+//   }
+//   console.log(obj);
+
+//  for (let j = 0; j < s.length; j++) {
+//     let char = s[j];
+//     if (obj[char] === 1) {
+//         return char;
+//     }
+// }
+// return null;
+// }
+
+// console.log
+// (firstUniqChar("leetcode"));
+
+//reverse a string
+
+function reverseString(s) {
+  if (s.length === 0) {
+    return console.log("String is empty");
+  }
+  let reverse = "";
+  for (let i = s.length - 1; i >= 0; i--) {
+    let char = s[i];
+    reverse += char;
+  }
+  return reverse;
+}
+console.log(reverseString("Dil Maange more"));
+
+//Move zeros (two pointer)
+
+let arr = [0, 1, 0, 3, 12];
+let insertPos = 0;
+
+for (let i = 0; i <= arr.length - 1; i++) {
+  if (arr[i] !== 0) {
+    arr[insertPos] = arr[i];
+    insertPos++;
+  }
+}
+
+for (let i = insertPos; i < arr.length; i++) {
+  arr[i] = 0;
+}
+
+console.log(arr);
+
 function firstUniqChar(s) {
   let obj = {};
 
-  for (i = 0; i < s.length; i++) {
+  for (let i = 0; i <= s.length - 1; i++) {
     let char = s[i];
     if (obj[char]) {
       obj[char]++;
@@ -47,16 +105,50 @@ function firstUniqChar(s) {
       obj[char] = 1;
     }
   }
-  console.log(obj);
+  // console.log(obj);
 
- for (let j = 0; j < s.length; j++) {
+  for (let j = 0; j <= s.length - 1; j++) {
     let char = s[j];
     if (obj[char] === 1) {
-        return char;
+      return char;
     }
-}
-return null;
+  }
 }
 
-console.log
-(firstUniqChar("leetcode"));
+console.log(firstUniqChar("leetcode"));
+
+function isAnagram(s, t) {
+  if(s.length !== t.length) return false
+
+  let obj = {}
+  for(let i = 0 ; i<s.length; i++){
+    let char = s[i]
+    // if(obj[char]){
+    //   obj[char]++
+    // }
+    // else{
+    //   obj[char] = 1;
+    // }
+
+    obj[char] = (obj[char] || 0 ) + 1
+
+  }
+  console.log(obj);
+  
+
+  for(let j = 0 ; j<t.length ; j++){
+    let char2 = t[j]
+    if (!obj[char2]) return false;
+    obj[char2]--;
+     if (obj[char2] < 0) return false;
+  }
+  console.log(obj);
+  
+  return true
+
+
+
+}
+
+console.log(isAnagram("listen", "silent"));//true hoga
+console.log(isAnagram("rat", "car")); // false
