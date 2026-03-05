84d855511bd88e33ede16fa15348f44e0669a931
alok-404 <alokchandra2621@gmail.com>
2026-03-05 20:57:31 +0530

    Arrays & Two Pointer Basics
    Practiced array fundamentals and revision
    
    Implemented reverse array using extra space
    
    Implemented in-place reverse array using two pointers
    
    Solved problem: move 0s to left and 1s to right
    
    Implemented left rotation of array by 1
    
    Implemented right rotation of array by 1
    
    Practiced swap logic and pointer movement
    
    Focused on understanding approach before looking at solution

--- array.js
+++ array.js
@@ -12,38 +12,78 @@
 // }
 // console.log(`Largest number in the array is ${max}`);
 
-// // Q2. Find the second largest number in an array
+// Q2. Find the second largest number in an array
 
-// //code
+//code
+// function secondLargestElement(aa){
 
-// const array = [3, 100, 2, 8, 27, 95, 90, 200,199]
-
-// if(array.length < 2){
-//   alert("Array Must be atleast of 2 character")
+// if (aa.length < 2) {
+//   alert("Array Must be atleast of 2 character");
 // }
-// let largest , secondLargest;
+// let largest = Math.max(aa[0], aa[1]);
+// let secondLargest = Math.min(aa[0], aa[1]);
 
-// if(array[0]>array[1]){
-//   largest = array[0]
-//   secondLargest = array[1]
-// }
-// else{
-//   largest = array[1]
-//   secondLargest = array[0]
-// }
+// console.log(largest, secondLargest);
 
-// for(let i = 2; i< array.length ; i++){
-//   let num = array[i]
-//   if(largest< num){
+// // if(array[0]>array[1]){
+// //   largest = array[0]
+// //   secondLargest = array[1]
+// // }
+// // else{
+// //   largest = array[1]
+// //   secondLargest = array[0]
+// // }
+
+// for (let i = 2; i < aa.length; i++) {
+//   let num = aa[i];
+//   if (largest < num) {
 //     secondLargest = largest;
-//     largest = num
-//   }
-//   else if(secondLargest < num){
-//     secondLargest = num
+//     largest = num;
+//   } else if (secondLargest < num && num != largest) {
+//     secondLargest = num;
 //   }
 // }
 
 // console.log(`Second Largest no. is ${secondLargest}`);
+// console.log(`Largest no. is ${largest}`);
+
+// }
+// secondLargestElement([3, 100, 2, 8, 27, 95, 90, 200, 199, 200, 200])
+
+// Q3. Find the second minimum and minimum number in an array
+
+// function secondMinElement(aa){
+
+// if (aa.length < 2) {
+//   alert("Array Must be atleast of 2 character");
+// }
+// let min = Math.min(aa[0], aa[1]);
+// let secondMin = Math.max(aa[0], aa[1]);
+
+// // if(array[0]>array[1]){
+// //   largest = array[0]
+// //   secondLargest = array[1]
+// // }
+// // else{
+// //   largest = array[1]
+// //   secondLargest = array[0]
+// // }
+
+// for (let i = 2; i < aa.length; i++) {
+//   let num = aa[i];
+//   if (min > num) {
+//     secondMin = min;
+//     min = num;
+//   } else if (secondMin > num && num != min) {
+//     secondmin = num;
+//   }
+// }
+
+// console.log(`Second Minimum no. is ${secondMin}`);
+// console.log(`Minimum no. is ${min}`);
+
+// }
+// secondMinElement([3, 100, 2, 8, 27, 95, 90, 200, 199, 200, 200])
 
 //11:20 am - 12:00 pe break 12:03 p3 start
 function twoSum(num, target) {
@@ -74,30 +114,28 @@ console.log(twoSum([2, 7, 11, 15], 9));
 //i = 2;
 //needed = target = num[i];
 // needed = 6 - 4 = 2
-// check krega 2 map mn hai = answer is yes ..toh return krdo map[needed] = 1 , 2, 
+// check krega 2 map mn hai = answer is yes ..toh return krdo map[needed] = 1 , 2,
 
-
-function bestBuyAndSell (arr){
+function bestBuyAndSell(arr) {
   let minPrice = arr[0];
   let maxProfit = 0;
 
-  for(let i = 1; i<arr.length; i++){
-    let profit = arr[i]- minPrice;
+  for (let i = 1; i < arr.length; i++) {
+    let profit = arr[i] - minPrice;
 
-    if(profit > maxProfit){
+    if (profit > maxProfit) {
       maxProfit = profit;
     }
 
-    if(arr[i] < minPrice){
-      minPrice = arr[i]
+    if (arr[i] < minPrice) {
+      minPrice = arr[i];
     }
   }
-return maxProfit
-
+  return maxProfit;
 }
 
-console.log(bestBuyAndSell([5,4,3,2,10]))
-console.log(bestBuyAndSell([2,1,2,1,0,1,2]))
+console.log(bestBuyAndSell([5, 4, 3, 2, 10]));
+console.log(bestBuyAndSell([2, 1, 2, 1, 0, 1, 2]));
 
 // minPrice = 2 , maxProfit = 0
 //p = 1-2 = -1
@@ -122,42 +160,40 @@ console.log(bestBuyAndSell([2,1,2,1,0,1,2]))
 // loop finish toh return hoga maxProfit = 2
 
 //sum of array;
-let arr = [10,20,30,40,50,4,6,186,1,2]
+let arr = [10, 20, 30, 40, 50, 4, 6, 186, 1, 2];
 let sum = 0;
-for(let i = 0 ; i<arr.length ; i++){
+for (let i = 0; i < arr.length; i++) {
   sum = sum + arr[i];
 }
 
 console.log(sum);
 
-
 // find Greatest Element And Index
 
 let max = arr[0];
-let index = 0
-for(let i = 1; i<arr.length; i++){
+let index = 0;
+for (let i = 1; i < arr.length; i++) {
   let num = arr[i];
-  if(max < num){
+  if (max < num) {
     max = num;
-    index = i
+    index = i;
   }
 }
 
-console.log(max,index);
+console.log(max, index);
 
 let min = arr[0];
-for(let i = 1; i<arr.length; i++){
+for (let i = 1; i < arr.length; i++) {
   let num = arr[i];
-  if(min > num){
+  if (min > num) {
     min = num;
   }
 }
 
 console.log(min);
 
-
 // function calculateSumAndMean(arr, n) {
-    // Write your logic here
+// Write your logic here
 //     let sum = 0;
 //     for(let i = 0; i<arr.length ; i++){
 //         sum = sum + arr[i]
@@ -171,17 +207,87 @@ console.log(min);
 const array = new Array(5).fill(0);
 
 for (let i = 0; i < 5; i++) {
-    console.log(array[i] + " ");
+  console.log(array[i] + " ");
 }
 
 let x = 50;
 
 if (x <= 50) {
-    if (x === 50) {
-        console.log("x is 50");
-    } else {
-        console.log("x is less than 50");
-    }
+  if (x === 50) {
+    console.log("x is 50");
+  } else {
+    console.log("x is less than 50");
+  }
 } else {
-    console.log("x is greater than 50");
+  console.log("x is greater than 50");
 }
+//two poiter algorithm
+
+function reverseArray(arr) {
+  let newArray = new Array(arr.length);
+  let i = 0;
+  let j = arr.length - 1;
+  while (j >= 0) {
+    newArray[i] = arr[j];
+    i++;
+    j--;
+  }
+  console.log(newArray);
+}
+reverseArray([1, 2, 3, 4, 5, 6]);
+
+//two pointer algorithm
+
+function reverseArrayWithoutUsingExtraSpace(arr) {
+  let i = 0,
+    j = arr.length - 1;
+  while (i < j) {
+    //if dono barabar hue toh swap krke whi milega and j ya fir i jda age nikal gye toh araay again cross hojayega
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+    i++;
+    j--;
+  }
+  return arr;
+}
+console.log(reverseArrayWithoutUsingExtraSpace([1, 2, 3, 4, 5, 6]));
+
+function shiftZeroesAndOne(arr) {
+  let i = 0;
+  j = 0;
+  while (i < arr.length) {
+    if (arr[i] === 0) {
+      let temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+      j++;
+    }
+    i++;
+  }
+
+  console.log(arr);
+}
+shiftZeroesAndOne([1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0]);
+
+function rotateEveryElemntLeftByOne(arr) {
+  let tp = arr[0];
+  for (let i = 0; i <= arr.length - 1; i++) {
+    arr[i] = arr[i + 1];
+  }
+  arr[arr.length - 1] = tp;
+
+  console.log(arr);
+}
+rotateEveryElemntLeftByOne([1, 2, 4, 6, 8, 10, 12]);
+
+function rotateEveryElemntRightByOne(arr) {
+  let tp = arr[arr.length - 1];
+  for (let i = arr.length - 1; i > 0 ; i--) {
+    arr[i] = arr[i - 1];
+  }
+  arr[0] = tp;
+
+  console.log(arr);
+}
+rotateEveryElemntRightByOne([1, 2, 4, 6, 8, 10, 12]);
